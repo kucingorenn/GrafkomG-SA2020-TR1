2802ce77e578d36f2a21957ed3ea2dc142df2a4b
Bimo Adam <65481065+kucingorenn@users.noreply.github.com>
2020-07-17 00:21:18 +0700

    Tambah Gedung Utama

--- GrafkomG-SA2020-TR1/Source.cpp
+++ GrafkomG-SA2020-TR1/Source.cpp
@@ -5,62 +5,168 @@ void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glBegin(GL_QUADS);
-	glColor3f(1.0, 0.0, 0.0); //DepanBawah
-	glVertex3f(-50, -25, 25);
-	glVertex3f(-50, 0, 50);
-	glVertex3f(50, 0, 50);
-	glVertex3f(50, -25, 25);
+	glColor3ub(178, 190, 158);
+	glVertex3f(-100, 0, -100);
+	glVertex3f(-100, 0, 100);
+	glVertex3f(100, 0, 100);
+	glVertex3f(100, 0, -100);
+	glEnd();
 
-	glColor3f(1.0, 1.0, 0.0); //DepanAtas
-	glVertex3f(-50, 25, 25);
-	glVertex3f(-50, 0, 50);
-	glVertex3f(50, 0, 50);
-	glVertex3f(50, 25, 25);
+	glBegin(GL_QUADS);
+	glColor3f(1.0, 0.0, 0.0); //Hall
+	glVertex3f(-10, 0, 25);
+	glVertex3f(-10, 5, 25);
+	glVertex3f(10, 5, 25);
+	glVertex3f(10, 0, 25);
 
-	glColor3f(0.0, 0.0, 0.0); //BelakangBawah
-	glVertex3f(-50, -25, -25);
-	glVertex3f(-50, 0, -50);
-	glVertex3f(50, 0, -50);
-	glVertex3f(50, -25, -25);
+	glVertex3f(-10, 0, 20);
+	glVertex3f(-10, 5, 20);
+	glVertex3f(10, 5, 20);
+	glVertex3f(10, 0, 20);
 
-	glColor3f(0.0, 1.0, 0.0); //BelakangAtas
-	glVertex3f(-50, 25, -25);
-	glVertex3f(-50, 0, -50);
-	glVertex3f(50, 0, -50);
-	glVertex3f(50, 25, -25);
+	glVertex3f(-10, 0, 20);
+	glVertex3f(-10, 5, 20);
+	glVertex3f(-10, 5, 25);
+	glVertex3f(-10, 0, 25);
 
-	glColor3f(0.0, 1.0, 1.0); //Atap
-	glVertex3f(-50, 25, 25);
-	glVertex3f(-50, 25, -25);
-	glVertex3f(50, 25, -25);
-	glVertex3f(50, 25, 25);
+	glVertex3f(10, 0, 20);
+	glVertex3f(10, 5, 20);
+	glVertex3f(10, 5, 25);
+	glVertex3f(10, 0, 25);
 
-	glColor3f(0.0, 0.0, 1.0); //Alas
-	glVertex3f(-50, -25, 25);
-	glVertex3f(-50, -25, -25);
-	glVertex3f(50, -25, -25);
-	glVertex3f(50, -25, 25);
+	glVertex3f(-10, 5, 25);
+	glVertex3f(-10, 5, 20);
+	glVertex3f(10, 5, 20);
+	glVertex3f(10, 5, 25);
+
+	glColor3f(1, 1, 0);
+	glVertex3f(-3, 0, 20); //LorongHall
+	glVertex3f(-3, 1, 20);
+	glVertex3f(-3, 1, 10);
+	glVertex3f(-3, 0, 10);
+
+	glVertex3f(3, 0, 20);
+	glVertex3f(3, 1, 20);
+	glVertex3f(3, 1, 10);
+	glVertex3f(3, 0, 10);
+
+	glVertex3f(-3, 1, 20);
+	glVertex3f(-3, 1, 10);
+	glVertex3f(3, 1, 10);
+	glVertex3f(3, 1, 20);
+
+	glVertex3f(-15, 0, 35); //GedungKecilKiri
+	glVertex3f(-15, 8, 35);
+	glVertex3f(-12, 8, 35);
+	glVertex3f(-12, 0, 35);
+
+	glVertex3f(-15, 0, 15);
+	glVertex3f(-15, 8, 15);
+	glVertex3f(-12, 8, 15);
+	glVertex3f(-12, 0, 15);
+
+	glVertex3f(-12, 0, 35);
+	glVertex3f(-12, 8, 35);
+	glVertex3f(-12, 8, 15);
+	glVertex3f(-12, 0, 15);
+
+	glVertex3f(-15, 0, 35);
+	glVertex3f(-15, 8, 35);
+	glVertex3f(-15, 8, 15);
+	glVertex3f(-15, 0, 15);
+
+	glVertex3f(-12, 8, 35);
+	glVertex3f(-15, 8, 35);
+	glVertex3f(-15, 8, 15);
+	glVertex3f(-12, 8, 15);
+
+	glColor3ub(200, 200, 200);
+	glVertex3f(-19, 0, 10); //GedungUtama
+	glVertex3f(-19, 15, 10);
+	glVertex3f(20, 15, 10);
+	glVertex3f(20, 0, 10);
+
+	glColor3ub(180, 180, 180);
+	glVertex3f(15, 0, 10);
+	glVertex3f(18, 0, 18);
+	glVertex3f(18, 15, 18);
+	glVertex3f(15, 15, 10);
+
+	glVertex3f(18, 0, 18);
+	glVertex3f(18, 15, 18);
+	glVertex3f(20, 15, 18);
+	glVertex3f(20, 0, 18);
+
+	glVertex3f(20, 0, 18);
+	glVertex3f(20, 15, 18);
+	glVertex3f(20, 15, -10);
+	glVertex3f(20, 0, -10);
+
+	glColor3ub(120, 120, 120);
+	glVertex3f(20, 0, -10);
+	glVertex3f(-19, 0, 10);
+	glVertex3f(-19, 15, 10);
+	glVertex3f(20, 15, -10);
+
+	glVertex3f(-18, 0, 10); //leftwing
+	glVertex3f(-18, 15, 10);
+	glVertex3f(-18, 15, 25);
+	glVertex3f(-18, 0, 25);
+
+	glColor3ub(160, 160, 160);
+	glVertex3f(-18, 0, 25);
+	glVertex3f(-18, 15, 25);
+	glVertex3f(-40, 15, 30);
+	glVertex3f(-40, 0, 30);
+
+	glVertex3f(-40, 0, 30);
+	glVertex3f(-40, 15, 30);
+	glVertex3f(-42, 15, 22);
+	glVertex3f(-42, 0, 22);
+
+	glVertex3f(-42, 0, 22);
+	glVertex3f(-42, 15, 22);
+	glVertex3f(-19, 15, 10);
+	glVertex3f(-19, 0, 10);
+
+	glColor3ub(150, 150, 150);
+	glVertex3f(-42, 15, 22);
+	glVertex3f(-40, 15, 30);
+	glVertex3f(-18, 15, 25);
+	glVertex3f(-18, 15, 9);
+
+	glColor3ub(190, 190, 190); //BelakangLorongBesar
+	glVertex3f(10, 0, 10);
+	glVertex3f(10, 15, 10);
+	glVertex3f(5, 15, -30);
+	glVertex3f(5, 0, -30);
+
+	glVertex3f(-5, 0, 10);
+	glVertex3f(-5, 15, 10);
+	glVertex3f(-10, 15, -30);
+	glVertex3f(-10, 0, -30);
+
+	glColor3ub(150, 150, 150);
+	glVertex3f(10, 15, 10);
+	glVertex3f(5, 15, -30);
+	glVertex3f(-10, 15, -30);
+	glVertex3f(-5, 15, 10);
 
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glColor3f(1.0, 0.5, 0.5); //Kanan
-	glVertex3f(50, 0, 50);
-	glVertex3f(50, 25, 25);
-	glVertex3f(50, 25, -25);
-	glVertex3f(50, 0, -50);
-	glVertex3f(50, -25, -25);
-	glVertex3f(50, -25, 25);
+	glColor3ub(150, 150, 150);
+	glVertex3f(15, 15, 10);
+	glVertex3f(18, 15, 18);
+	glVertex3f(20, 15, 18);
+	glVertex3f(20, 15, 10);
 	glEnd();
 
-	glBegin(GL_POLYGON);
-	glColor3f(1.0, 0.0, 1.0); //Kiri
-	glVertex3f(-50, 0, 50);
-	glVertex3f(-50, 25, 25);
-	glVertex3f(-50, 25, -25);
-	glVertex3f(-50, 0, -50);
-	glVertex3f(-50, -25, -25);
-	glVertex3f(-50, -25, 25);
+	glBegin(GL_TRIANGLES);
+	glColor3ub(150, 150, 150);
+	glVertex3f(20, 15, 10);
+	glVertex3f(20, 15, -10);
+	glVertex3f(-19, 15, 10);
 	glEnd();
 
 	glPushMatrix();
@@ -69,11 +175,12 @@ void Display(void) {
 }
 
 void myinit(void) {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glEnable(GL_DEPTH_TEST);
 	glColor3f(1.0, 0.0, 0.0);
 	glPointSize(2.0);
-	//glMatrixMode(GL_PROJECTION);
+	glShadeModel(GL_SMOOTH);
+	//glMatrixMode(GL_MODELVIEW);
 	//glLoadIdentity();
 	glOrtho(-100, 100, -100, 100, -100, 100);
 }
